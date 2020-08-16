61cab25e4cab37aae04c0050d9b3942a0e9a6e73
Johnny Lieu <johnnylieu@yahoo.com>
2020-08-16 10:39:50 -0700

    stars approved on

--- Assets/Scripts/app.js
+++ Assets/Scripts/app.js
@@ -188,7 +188,7 @@ function callback(results, status) {
 
             // displays names and ratings with link to get directions
             cardAction.append("<h6><strong>" + name + "</strong></h6>");
-            cardAction.append("<p id='stars'>" + getStars() + ratings  + "</p>");
+            cardAction.append("<p id='stars'>" + ratings + "  " + getStars()  + "</p>");
             cardAction.append("<p><a target='_blank' href = https://www.google.com/maps/search/?api=1&query=" + latYo + "," + longYo + ">" + "Directions" + "</a></p>");
             cardAction.append("<p><a target='_blank' href = https://search.google.com/local/reviews?placeid=" + placesId + ">" + "Reviews" + "</a></p>");
             cardAction.append("<hr>");
